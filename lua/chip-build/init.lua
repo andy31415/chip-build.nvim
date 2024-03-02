b3bddce5da77adc13012e02ecaaf8d4784cda7d9
Andrei Litvin <andy314@gmail.com>
2024-03-02 11:23:53 -0500

    expose a command now

--- lua/chip-build/init.lua
+++ lua/chip-build/init.lua
@@ -20,9 +20,25 @@ M.setup = function(options)
   if not options then
     options = {}
   end
-  vim.api.nvim_set_keymap('n', '<Space>tt', "<CMD>lua require('chip-build').test()<CR>", { noremap = true })
+
+  vim.api.nvim_create_user_command("ChipBuild", function(opts)
+    local cb = require('chip-build')
+    local cmd = opts.args
+    if cmd == "test" then
+      cb.test()
+    elseif cmd == "devel_reset" then
+      cb.devel_reset()
+    else
+      print(string.format("Unknown chip-build command: %s", cmd))
+    end
+  end, {
+    nargs = 1
+  })
+
+
+  vim.api.nvim_set_keymap('n', '<Space>tt', "<CMD>ChipBuild test<CR>", { noremap = true })
   if options.development or false then
-    vim.api.nvim_set_keymap('n', '<Space>tr',  "<CMD>lua require('chip-build').devel_reset()<CR>", { noremap = true })
+    vim.api.nvim_set_keymap('n', '<Space>tr', "<CMD>ChipBuild devel_reset<CR>", { noremap = true })
   end
 end
 
