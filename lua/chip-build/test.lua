acdab1cdb68a89b5bbfbbaa66e975498e461ebb8
Andrei Litvin <andy314@gmail.com>
2025-05-07 09:14:46 -0400

    Some restyle and add one more unit test

--- lua/chip-build/test.lua
+++ lua/chip-build/test.lua
@@ -1,121 +1,156 @@
 -- This file should be run via `busted -m lua`
 
-local targets = require('chip-build.targets')
+local targets = require("chip-build.targets")
 
 describe("Target iteration works", function()
-  local test_targets = {
-    {
-      prefixes = { 'foo', { 'a', 'b', 'c' }, { 'x', 'y' } },
-      suffixes = { 'bar', 'baz' }
-    },
-    {
-      prefixes = { 'test', { '1', '2', '2' } },
-      suffixes = {}
-    }
-  }
+	local test_targets = {
+		{
+			prefixes = { "foo", { "a", "b", "c" }, { "x", "y" } },
+			suffixes = { "bar", "baz" },
+		},
+		{
+			prefixes = { "test", { "1", "2", "2" } },
+			suffixes = {},
+		},
+	}
 
-  local next_choice = function(c)
-    local next, _ = targets.next_component_choices(c, { targets = test_targets })
-    return next
-  end
+	local next_choice = function(c)
+		local next, _ = targets.next_component_choices(c, { targets = test_targets })
+		return next
+	end
 
-  it("shows top level items", function()
-    assert.are.same(next_choice({}), { 'foo', 'test' })
-  end)
+	it("allows final first element", function()
+		assert.are.same(targets.split_target_string("my-test[-foo][-bar]"), {
+			prefixes = { "my-test" },
+			suffixes = { "foo", "bar" },
+		})
 
-  it("can get prefixes", function()
-    assert.are.same(next_choice({ 'foo' }), { 'a', 'b', 'c' })
-  end)
+		local next, final = targets.next_component_choices(
+			{},
+			{ targets = { {
+				prefixes = { "my-test" },
+				suffixes = { "foo", "bar" },
+			} } }
+		)
 
-  it("can get suffixes", function()
-    assert.are.same(next_choice({ 'foo', 'a', 'x' }), { 'bar', 'baz' })
+		assert.are.same({ "my-test" }, next)
+		assert.is_false(final)
 
-    -- suffixes are trimmed
-    assert.are.same(next_choice({ 'foo', 'a', 'x', 'bar' }), { 'baz' })
-    assert.are.same(next_choice({ 'foo', 'a', 'x', 'bar', 'baz' }), {})
-    assert.are.same(next_choice({ 'test', '1' }), {})
-  end)
+		next, final = targets.next_component_choices(
+			{ "my-test" },
+			{ targets = { {
+				prefixes = { "my-test" },
+				suffixes = { "foo", "bar" },
+			} } }
+		)
+
+		assert.are.same({ "bar", "foo" }, next)
+		assert.is_true(final)
+	end)
+
+	it("shows top level items", function()
+		assert.are.same(next_choice({}), { "foo", "test" })
+	end)
+
+	it("can get prefixes", function()
+		assert.are.same(next_choice({ "foo" }), { "a", "b", "c" })
+	end)
+
+	it("can get suffixes", function()
+		assert.are.same(next_choice({ "foo", "a", "x" }), { "bar", "baz" })
+
+		-- suffixes are trimmed
+		assert.are.same(next_choice({ "foo", "a", "x", "bar" }), { "baz" })
+		assert.are.same(next_choice({ "foo", "a", "x", "bar", "baz" }), {})
+		assert.are.same(next_choice({ "test", "1" }), {})
+	end)
 end)
 
 describe("String splitting", function()
-  it("splits strings", function()
-    assert.are.same(targets.split_lines("test\nbar"), { 'test', 'bar' })
-    assert.are.same(targets.split_lines("\ntest\n\n\nbar\n"), { 'test', 'bar' })
+	it("splits strings", function()
+		assert.are.same(targets.split_lines("test\nbar"), { "test", "bar" })
+		assert.are.same(targets.split_lines("\ntest\n\n\nbar\n"), { "test", "bar" })
 
-    assert.are.same(targets.split_lines([[
+		assert.are.same(
+			targets.split_lines([[
 this-is
 what-is-used
-    ]]), { 'this-is', 'what-is-used' })
-    assert.are.same(targets.split_lines([[
+    ]]),
+			{ "this-is", "what-is-used" }
+		)
+		assert.are.same(
+			targets.split_lines([[
        ignores
        begin
        and-end
        whitespace
        embedded whitepace is ok
-    ]]), {
-      'ignores',
-      'begin',
-      'and-end',
-      'whitespace',
-      'embedded whitepace is ok'
-    })
-  end)
+    ]]),
+			{
+				"ignores",
+				"begin",
+				"and-end",
+				"whitespace",
+				"embedded whitepace is ok",
+			}
+		)
+	end)
 end)
 
 describe("target_split", function()
-  it('splits the start', function()
-    assert.are.same(targets.split_target_string('foo'), {
-      prefixes = { 'foo' },
-      suffixes = {},
-    })
-    assert.are.same(targets.split_target_string('bar-'), {
-      prefixes = { 'bar' },
-      suffixes = {},
-    })
-    assert.are.same(targets.split_target_string('bar-baz'), {
-      prefixes = { 'bar-baz' },
-      suffixes = {},
-    })
-    assert.are.same(targets.split_target_string('bar-baz-'), {
-      prefixes = { 'bar-baz' },
-      suffixes = {},
-    })
-  end)
+	it("splits the start", function()
+		assert.are.same(targets.split_target_string("foo"), {
+			prefixes = { "foo" },
+			suffixes = {},
+		})
+		assert.are.same(targets.split_target_string("bar-"), {
+			prefixes = { "bar" },
+			suffixes = {},
+		})
+		assert.are.same(targets.split_target_string("bar-baz"), {
+			prefixes = { "bar-baz" },
+			suffixes = {},
+		})
+		assert.are.same(targets.split_target_string("bar-baz-"), {
+			prefixes = { "bar-baz" },
+			suffixes = {},
+		})
+	end)
 
-  it('splits groups', function()
-    assert.are.same(targets.split_target_string('foo-{a,b}'), {
-      prefixes = { 'foo', { 'a', 'b' } },
-      suffixes = {},
-    })
-    assert.are.same(targets.split_target_string('foo-{a,b}-{x,yz1,a-test}'), {
-      prefixes = { 'foo', { 'a', 'b' }, { 'x', 'yz1', 'a-test' } },
-      suffixes = {},
-    })
-  end)
+	it("splits groups", function()
+		assert.are.same(targets.split_target_string("foo-{a,b}"), {
+			prefixes = { "foo", { "a", "b" } },
+			suffixes = {},
+		})
+		assert.are.same(targets.split_target_string("foo-{a,b}-{x,yz1,a-test}"), {
+			prefixes = { "foo", { "a", "b" }, { "x", "yz1", "a-test" } },
+			suffixes = {},
+		})
+	end)
 
-  it('splits suffixes', function()
-    assert.are.same(targets.split_target_string('foo-{a,b}[-foo]'), {
-      prefixes = { 'foo', { 'a', 'b' } },
-      suffixes = { 'foo' },
-    })
-    assert.are.same(targets.split_target_string('foo-{a,b}[-foo][-bar]'), {
-      prefixes = { 'foo', { 'a', 'b' } },
-      suffixes = { 'foo', 'bar' },
-    })
-    assert.are.same(targets.split_target_string('no-groups[-foo][-bar]'), {
-      prefixes = { 'no-groups' },
-      suffixes = { 'foo', 'bar' },
-    })
-  end)
+	it("splits suffixes", function()
+		assert.are.same(targets.split_target_string("foo-{a,b}[-foo]"), {
+			prefixes = { "foo", { "a", "b" } },
+			suffixes = { "foo" },
+		})
+		assert.are.same(targets.split_target_string("foo-{a,b}[-foo][-bar]"), {
+			prefixes = { "foo", { "a", "b" } },
+			suffixes = { "foo", "bar" },
+		})
+		assert.are.same(targets.split_target_string("no-groups[-foo][-bar]"), {
+			prefixes = { "no-groups" },
+			suffixes = { "foo", "bar" },
+		})
+	end)
 
-  it('splits with end', function()
-    assert.are.same(targets.split_target_string('x-{a,b}-foo[-bar][-foo]'), {
-      prefixes = { 'x', { 'a', 'b' }, 'foo' },
-      suffixes = { 'bar', 'foo' },
-    })
-    assert.are.same(targets.split_target_string('x-{a,b}-{foo}[-bar][-foo]'), {
-      prefixes = { 'x', { 'a', 'b' }, { 'foo' } },
-      suffixes = { 'bar', 'foo' },
-    })
-  end)
+	it("splits with end", function()
+		assert.are.same(targets.split_target_string("x-{a,b}-foo[-bar][-foo]"), {
+			prefixes = { "x", { "a", "b" }, "foo" },
+			suffixes = { "bar", "foo" },
+		})
+		assert.are.same(targets.split_target_string("x-{a,b}-{foo}[-bar][-foo]"), {
+			prefixes = { "x", { "a", "b" }, { "foo" } },
+			suffixes = { "bar", "foo" },
+		})
+	end)
 end)
